573eb12208c3e3894a6232680d980c1d361d557c
Tu Vu <anhtu23011996@gmail.com>
2020-12-04 19:04:33 +0700

    set up to deploy to heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const app = express();
+
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(["https://", req.get("Host"), req.url].join(""));
+    }
+    next();
+  };
+};
+app.use(express.static("./dist/fe47-angular"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
+
+app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);
