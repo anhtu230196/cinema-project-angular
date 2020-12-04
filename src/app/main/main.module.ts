564705abfc72ecb30b9066800ae758ef8f4a29ca
Tu Vu <anhtu23011996@gmail.com>
2020-12-04 22:08:28 +0700

    Revert "demo"
    This reverts commit 38a3fad8eac7f6c7ec1b8f71d963d926d1257a58.

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -32,7 +32,6 @@ import { ModalWarningComponent } from "./home/modal/modal-warning/modal-warning.
 import { ChiTietRateComponent } from "./movie-detail/chi-tiet-rate/chi-tiet-rate.component";
 import { DanhSachGheComponent } from "./trang-dat-ve/danh-sach-ghe/danh-sach-ghe.component";
 import { GheItemComponent } from "./trang-dat-ve/danh-sach-ghe/ghe-item/ghe-item.component";
-import { UserInfoComponent } from './user-info/user-info.component';
 
 const routes: Routes = [
   {
@@ -77,7 +76,6 @@ const routes: Routes = [
     ChiTietRateComponent,
     DanhSachGheComponent,
     GheItemComponent,
-    UserInfoComponent,
   ],
   imports: [
     CommonModule,
